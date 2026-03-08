36dad13bec3db7da00142fbe942bd807eacf3c59
tianer2820 <tianer2820@outlook.com>
2026-03-08 14:14:19 -0400

    add heating pin defs

--- rotation-stage-controller/src/config.h
+++ rotation-stage-controller/src/config.h
@@ -14,14 +14,13 @@
 
 
 
-
 /**
  * PWM
  */
 
 // The 72MHz clock cannot support 40kHz with 12-bit resolution, so the actual frequency will be lower
 #define PWM_FREQ 40000
-#define PWM_RESOLUTION RESOLUTION_12B_COMPARE_FORMAT
+#define PWM_RES RESOLUTION_12B_COMPARE_FORMAT
 #define PWM_MAX_VALUE 4096
 
 #define MOT_A_POS PA_0
@@ -39,12 +38,18 @@
 #define I2C_SCL PB6
 
 
+
 /**
  * Endstop & homing
  */
 #define ROTATION_ENDSTOP PA12
 
 
+/**
+ * Heating
+ */
+#define TEMP_SENSOR_PIN PB0
+#define HEATER_PIN PB1
 
 /**
  * Control
